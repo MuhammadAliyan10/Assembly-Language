22600024825ef27f16f545bb1c12fbde0b8580b0
MuhammadAliyan10 <135156764+MuhammadAliyan10@users.noreply.github.com>
2023-12-21 10:59:34 +0500

    Added

---
+++ Loop/oneToTen.asm
@@ -0,0 +1,29 @@
+dosseg
+.model small
+.stack 100h
+.data
+.code 
+main proc
+    mov cx,10
+    mov dx,48
+    L1:
+    mov ah,2
+    int 21h
+    add dx,1
+    loop L1
+    mov dx,10
+    mov ah,2
+    int 21h
+    mov cx,10
+    mov dx,57
+    L2:
+    mov ah,2
+    int 21h
+    sub dx,1
+    loop L2
+        mov ah,4ch
+        int 21h
+    
+        
+main endp
+end main
